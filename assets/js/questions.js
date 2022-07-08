f6eafe3d90720824703e5062c6c1ed804ababe42
RyanNazario0357 <Ryan0357@icloud.com>
2022-07-07 20:11:37 -0400

    web-api

---
+++ assets/js/questions.js
@@ -0,0 +1,46 @@
+// array of the questions and answers for the quiz
+
+var questions = [{
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["js", "javaScript", "script", "scripting"],
+    answer: "script"
+  },
+
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: ["Both the <head> section and the <body> section are correct", "The <body> section",
+      "The <head> section"
+    ],
+    answer: "Both the <head> section and the <body> section are correct"
+  },
+
+  {
+    title: "An external JavaScript must contain the <script> tag",
+    choices: ["True", "False", ],
+    answer: "False"
+  },
+
+  {
+    title: "How do you create a function?",
+    choices: ["function:myFunction()", "function=myFunction()",
+      "function myFunction()", "myFunction():function"
+    ],
+    answer: "function myFunction()"
+  },
+
+  {
+    title: "How do you declare a variable on JavaScript?",
+    choices: ["v variableName", "variable variableName",
+      "let var variableName", "var variableName"
+    ],
+    answer: "var variableName"
+  },
+
+  {
+    title: "How can you add a comment in a JavaScript?",
+    choices: ["//This is a comment", "'This is a comment",
+      "<!--This is a comment-->", "#This is a comment"
+    ],
+    answer: "//This is a comment"
+  }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,143 @@
+// Dom Elements
+var quizTimeEl = document.querySelector("#quizTime");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var startQuizBtn = document.querySelector("#startQuiz");
+var submitBtn = document.querySelector("#submit");
+var initialsEl = document.querySelector("#initials");
+var answerResultEl = document.querySelector("#answerResult");
+
+// changing variables depending on amount of questions
+var ongoingQuestionArray = 0;
+var time = questions.length * 10;
+var timerId;
+
+
+function startCodeQuiz() {
+  //  We want to hide the home screen
+  var homeScreenEl = document.getElementById("homeScreen");
+  homeScreenEl.setAttribute("class", "hidden");
+
+  // Next we will make the questiosn section visible 
+  questionsEl.removeAttribute("class");
+
+  // Timer will start counting down
+  timerId = setInterval(timerValue, 1000);
+
+  quizTimeEl.textContent = time;
+
+  pullQuestions();
+}
+
+function pullQuestions() {
+  // will pull the questions from its array
+  var ongoingQuestion = questions[ongoingQuestionArray];
+
+  // pulled questioned will appear on screen
+  var questionTitleEl = document.getElementById("questionTitle");
+  questionTitleEl.textContent = ongoingQuestion.title;
+
+  // remove any of the old chocies available 
+  choicesEl.innerHTML = "";
+
+  // chnaged the choices for the new questions
+  ongoingQuestion.choices.forEach(function(choice, i) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+    choiceNode.onclick = choiceSelection;
+
+    // display new  choices on html page
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+function choiceSelection() {
+  // first check for wrong answer
+  if (this.value !== questions[ongoingQuestionArray].answer) {
+    time = time - 10;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    // show new time on page 
+    quizTimeEl.textContent = time;
+    answerResultEl.textContent = "Incorrect";
+    answerResultEl.style.color = "#BC4123";
+    answerResultEl.style.fontSize = "15px";
+  } else {
+    answerResultEl.textContent = "Correct";
+    answerResultEl.style.color = "#BC4123";
+    answerResultEl.style.fontSize = "15px";
+  }
+
+  // quickly show answer result if right or wrong
+  answerResultEl.setAttribute("class", "answerResult");
+  setTimeout(function() {
+    answerResultEl.setAttribute("class", "answerResult hidden");
+  }, 1000);
+
+  // next question appears
+  ongoingQuestionArray++;
+
+  if (ongoingQuestionArray === questions.length) {
+    endQuiz();
+  } else {
+    pullQuestions();
+  }
+}
+
+function endQuiz() {
+  clearInterval(timerId); // this will stop the timer
+
+  // Quiz COmplete screen will appear 
+  var quizCompleteEl = document.getElementById("quizComplete");
+  quizCompleteEl.removeAttribute("class");
+
+  // score will appear
+  var scoreEl = document.getElementById("score");
+  scoreEl.textContent = time;
+
+  // move the questionsEl back to hidden
+  questionsEl.setAttribute("class", "hidden");
+}
+
+function timerValue() {
+  time--;
+  quizTimeEl.textContent = time;
+
+  if (time <= 0) {
+    endQuiz();
+  }
+}
+
+function saveScore() {
+  // save initials
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+    var scores = JSON.parse(window.localStorage.getItem("scores")) || []; // will pull empty array if their are no scores in local storage
+
+    // if new score
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    // save score to localStorage
+    scores.push(newScore);
+    window.localStorage.setItem("scores", JSON.stringify(scores));
+
+    // take to the highscores.html page
+    window.location.href = "./highscores.html";
+  }
+}
+
+// Start Code Quiz
+startQuizBtn.onclick = startCodeQuiz;
+
+
+submitBtn.onclick = saveScore;
